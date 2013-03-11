7e6d153bf8d25efa24668082c0a8b92e002b5e9c
Nikhil Handigol <nikhilh@stanford.edu>
2013-03-11 01:17:42 -0700

    starting the ndb app

---
+++ netsight/apps/ndb/main.cc
@@ -0,0 +1,13 @@
+#include <pthread.h>
+
+#include "api.hh"
+#include "types.hh"
+#include "helper.hh"
+#include "postcard.hh"
+#include "packet.hh"
+
+
+int main(int argc, char **argv)
+{
+    return 0;
+}
